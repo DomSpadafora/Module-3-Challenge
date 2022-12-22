7f0fe18d4ad0b211e8eeacdac0abb20942ff162c
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2022-12-22 17:20:00 -0500

    changes made up to special characters

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+const passGen = () => {
+    let len = prompt('How long would you like your password to be (8-128)?');
+    if(len < 8 || len > 128){
+        alert('Password needs to be between 8-128.');
+        return passGen();
+    };
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    let temp='';
+    let password='';
 
-  passwordText.value = password;
+    let num = confirm('Would you like your password to have number?');
+    if(num) temp+='0123456789';
 
-}
+    let lower = confirm('Would you like your password to have lower case?');
+    if(lower) temp+='abcdefghijklmnopqrstuvwxyz';
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    let upper = confirm('Would you like your password to have upper case?');
+    if(upper) temp+='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    let special = confirm('Would you like your password to have special characters?');
+    if(special) temp+='!$@%_>?<*';
+
+
+    for (let i = 0; i < len; i++) {
+        password+=temp[Math.floor(Math.random()*temp.length)]
+    };
+
+    document.querySelector('textarea').innerHTML = password;
+};
+
+document.querySelector('button').addEventListener('click', passGen)
+
